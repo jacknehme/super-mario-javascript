8c1289ab97acf73ccf69f1cf5e47607d121cc288
Jack Nehme <jack.nehme@gmail.com>
2017-12-28 22:08:12 -0800

    WIP: adding keyboard input

--- public/js/Entity.js
+++ public/js/Entity.js
@@ -1,8 +1,31 @@
 import { Vec2 } from './math.js';
 
+export class Trait {
+    constructor(name) {
+        this.name = name;
+    }
+
+    update() {
+        console.warn('Unhandled update call in Trait');
+    }
+}
+
 export default class Entity {
     constructor() {
         this.pos = new Vec2(0, 0);
         this.vel = new Vec2(0, 0);
+
+        this.traits = [];
+    }
+
+    addTrait(trait) {
+        this.traits.push(trait);
+        this[trait.NAME] = trait;
+    }
+
+    update(deltaTime) {
+        this.traits.forEach(traits => {
+            traits.update(this, deltaTime);
+        });
     }
 }
