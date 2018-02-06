e00883994796912b01de65cde31c968d7f12b4ef
Jack Nehme <jack.nehme@gmail.com>
2018-02-05 22:00:11 -0800

    Ep 10 - Synchronous Entity Spawning

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,41 @@
 import Camera from './Camera.js';
 import Timer from './timer.js';
 import { loadLevel } from './loaders/level.js';
-import { createMario } from './entities.js';
+import { loadMario } from './entities/Mario.js';
 import { setupKeyboard } from './input.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
-    createMario(),
+    loadMario(),
     loadLevel('1-1'),
-]).then(([mario, level]) => {
+]).then(([createMario, level]) => {
 
     const camera = new Camera();
     window.camera = camera;
 
+    const mario = createMario();
     mario.pos.set(64, 64);
+
+    // mario.addTrait({
+    //     NAME: 'hacktrait',
+    //     spawnTimeout: 0,
+    //     obstruct() {
+
+    //     },
+    //     update(mario, deltaTime) {
+    //         if (this.spawnTimeout > 0.1 && mario.vel.y < 0) {
+    //             const spawn = createMario();
+    //             spawn.pos.x = mario.pos.x;
+    //             spawn.pos.y = mario.pos.y;
+    //             spawn.vel.y = mario.vel.y - 200;
+    //             level.entities.add(spawn);
+    //             this.spawnTimeout = 0;
+    //         }
+    //         this.spawnTimeout += deltaTime;
+    //     }
+    // });
 
     level.entities.add(mario);
 
