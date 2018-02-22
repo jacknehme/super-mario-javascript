8c85e9277c02d73d6a1bb7fa2d385c39bae2494d
Jack Nehme <jack.nehme@gmail.com>
2018-02-21 21:31:21 -0800

    Adding collision behavior for koopa

--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,4 +1,5 @@
-import Entity, { Sides } from '../Entity.js';
+import Entity, { Sides, Trait } from '../Entity.js';
+import Killable from '../traits/Killable.js';
 import PendulumWalk from '../traits/PendulumWalk.js'
 import { loadSpriteSheet } from '../loaders.js';
 
@@ -7,11 +8,106 @@ export function loadKoopa() {
         .then(createKoopaFactory)
 }
 
+const STATE_WALKING = Symbol('walking');
+const STATE_HIDING = Symbol('hiding');
+const STATE_PANIC = Symbol('panic');
+
+
+class Behavior extends Trait {
+    constructor() {
+        super('behavior');
+        this.hideTime = 0;
+        this.hideDuration = 5;
+
+        this.walkSpeed = null;
+        this.panicSpeed = 300;
+
+        this.state = STATE_WALKING;
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+        if (them.stomper) {
+            if (them.vel.y > us.vel.y) {
+                this.handleStomp(us, them);
+            } else {
+                this.handleNudge(us, them);
+            }
+        }
+    }
+
+    handleNudge(us, them) {
+        if (this.state === STATE_WALKING) {
+            them.killable.kill();
+        } else if (this.state === STATE_HIDING) {
+            this.panic(us, them);
+        } else if (this.state === STATE_PANIC) {
+            const travelDir = Math.sign(us.vel.x);
+            const impactDir = Math.sign(them.pos.x - them.pos.x);
+            if (travelDir !== 0 && travelDir !== impactDir) {
+                them.killable.kill();
+            }
+        }
+    }
+
+    handleStomp(us, them) {
+        if (this.state === STATE_WALKING) {
+            this.hide(us);
+        } else if (this.state === STATE_HIDING) {
+            us.killable.kill();
+            us.vel.set(100, -200);
+            us.canCollide = false;
+        } else if (this.state === STATE_PANIC) {
+            this.hide(us);
+        }
+    }
+
+    hide(us) {
+        us.vel.x = 0;
+        us.pendulumWalk.enabled = false;
+        if (this.walkSpeed === null) {
+            this.walkSpeed = us.pendulumWalk.speed;
+        }
+        this.hideTime = 0;
+        this.state = STATE_HIDING;
+    }
+
+    unhide(us) {
+        us.pendulumWalk.enabled = true;
+        us.pendulumWalk.speed = this.walkSpeed;
+        this.state = STATE_WALKING;
+    }
+
+    panic(us, them) {
+        us.pendulumWalk.enabled = true;
+        us.pendulumWalk.speed = this.panicSpeed * Math.sign(them.vel.x);
+        this.state = STATE_PANIC;
+    }
+
+    update(us, deltaTime) {
+        if (this.state === STATE_HIDING) {
+            this.hideTime += deltaTime;
+            if (this.hideTime > this.hideDuration) {
+                this.unhide(us);
+            }
+        }
+    }
+}
+
 function createKoopaFactory(sprite) {
     const walkAnim = sprite.animations.get('walk');
 
+    function routeAnim(koopa) {
+        if (koopa.behavior.state === STATE_HIDING || koopa.behavior.state === STATE_PANIC) {
+            return 'hiding';
+        }
+        return walkAnim(koopa.lifetime);
+    }
+
     function drawKoopa(context) {
-        sprite.draw(walkAnim(this.lifetime), context, 0, 0, this.vel.x < 0);
+        sprite.draw(routeAnim(this), context, 0, 0, this.vel.x < 0);
     }
 
     return function createKoopa() {
@@ -20,6 +116,8 @@ function createKoopaFactory(sprite) {
         koopa.offset.y = 8;
 
         koopa.addTrait(new PendulumWalk());
+        koopa.addTrait(new Killable());
+        koopa.addTrait(new Behavior())
 
         koopa.draw = drawKoopa;
 
